641b7983fab7701a97b246463d2ba61dda688101
The Punisher <contact@lemarcel.net>
2025-04-12 09:15:41 +0100

    adding absolute path for staticfiles

--- main/settings.py
+++ main/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'assets'
+STATIC_ROOT = '/home/estigect/data_api_cif/assets'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
