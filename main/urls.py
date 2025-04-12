372917bea7fbe6a2edf21bdc346f0dae517dcd7f
The Punisher <contact@lemarcel.net>
2025-04-12 09:21:48 +0100

    default principal redirect is admin

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
-    path("ckeditor5/", include('django_ckeditor_5.urls')),
+    path('api/', include('api.urls')),
+    path('ckeditor5/', include('django_ckeditor_5.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
